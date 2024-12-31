552892586de14602bc49209ff657e749dc92666d
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-31 17:55:59 +0700

    fix understanding

--- app/(home)/learning-materials/understanding/page.tsx
+++ app/(home)/learning-materials/understanding/page.tsx
@@ -1,19 +1,17 @@
-import { getCurrentUser } from '@/actions/getCurrentUser';
-import { ButtonHome } from '@/components/button-home';
 import { Metadata } from 'next';
 import { CardUnderstanding } from './card-understanding';
+import { OnlyHome } from '@/components/only-home';
 
 export const metadata: Metadata = {
   title: 'Materi',
 };
 
 export default async function Understanding() {
-  const currentUser = await getCurrentUser();
   return (
     <>
       <div className="fixed inset-0 bg-relevant bg-center bg-cover bg-no-repeat sm:bg-home_two_wide -z-10" />
       <section className="relative min-h-screen">
-        <ButtonHome currentUser={currentUser} />
+        <OnlyHome />
         <CardUnderstanding />
       </section>
     </>
