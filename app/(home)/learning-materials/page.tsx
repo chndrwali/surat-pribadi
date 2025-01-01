5749a871c36b52cbb538e29dc63a9d4a483a4a7c
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-01-01 17:19:46 +0700

    add: pro

--- app/(home)/learning-materials/page.tsx
+++ app/(home)/learning-materials/page.tsx
@@ -3,6 +3,7 @@ import { ButtonHome } from '@/components/button-home';
 import { Metadata } from 'next';
 import { Navigation } from './navigation';
 import { Logo } from '../logo';
+import { HomeLeft } from '../home/home-left';
 
 export const metadata: Metadata = {
   title: 'Materi',
@@ -13,7 +14,8 @@ export default async function LearningMaterials() {
   return (
     <section className="relative bg-relevant bg-cover bg-center h-[100vh] sm:bg-home_two_wide">
       <Logo />
-      <ButtonHome currentUser={currentUser} />
+      <HomeLeft />
+      <ButtonHome currentUser={currentUser} hide />
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-6xl font-bold text-white mt-[50px]" style={{ textShadow: '2px 2px 0 #0000ff' }}>
           Menu Materi
